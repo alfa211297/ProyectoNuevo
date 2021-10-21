205bca6b887d1f890df9bda9651f536ce3708446
alfa211297 <ae15007@ues.edu.sv>
2021-10-21 09:33:34 -0600

    version agregado de uniformes

---
+++ victor.php
@@ -0,0 +1,21 @@
+<!doctype html>  
+<html>
+	<head>
+	<meta charset="utf-8">
+		<title>emekjejekle</title>  
+	</head>
+	<body>
+		<?php
+		$nombre1="Juan";
+		$edad=18;
+		//print $nombre1;
+		//print $edad;
+		echo "el nombre es: ".$nombre1. " y tiene ".$edad." años de edad </br>";
+		
+		function dameDatos(){
+			echo "Este mensaje esta dentro de la funcion </br>";
+		}
+		dameDatos();
+	?>
+	</body>
+</html>
